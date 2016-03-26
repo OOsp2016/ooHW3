9a4de4b20220ee08acff95ed4f6bfd0ce2c022f7
Jesusmann <Jesusmann46and2@gmail.com>
2016-03-25 21:32:09 -0500

    Added front end choice
    added frontend choice swtich statement

--- src/edu/txstate/simpleLibJJP66/FrontEnd.java
+++ src/edu/txstate/simpleLibJJP66/FrontEnd.java
@@ -1,6 +1,7 @@
 package edu.txstate.simpleLibJJP66;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class FrontEnd {
 
@@ -12,5 +13,44 @@ public class FrontEnd {
 				+ "2. Switch to librarian view. \n"
 				+ "3. Switch to Admin view. \n" 
 				+ "4. Exit program.");
+	}
+	
+	public static void start() {
+		int choice = 0;
+		boolean exitProgram = false;
+		do {
+			introMenu();
+			try {
+				choice = in.nextInt();
+				in.nextLine(); // clear the new line character from the input
+
+				switch (choice) {
+				case 1:
+					UserView.UVMenuChoice();
+					break;
+				case 2:
+					//switchToLibrarianView();
+					break;
+				case 3:
+					//switchToAdminView();
+					break;
+				case 4:
+					//saveAndQuit();
+					exitProgram = true;
+					break;
+				default:
+					System.err
+							.println("Please select a number between 1 and 4.");
+				}
+			} catch (InputMismatchException ex) {
+				System.err.println("Input missmatch. Please Try again." + ex);
+				in.nextLine(); // clear the new line character from the input
+			} catch (UnsupportedOperationException ex) {
+				ex.printStackTrace(); // For debugging purposes only.
+			} catch (Exception ex) {
+				System.err.println("An unknown error has occured." + ex);
+			}
+		} while (!exitProgram);
+
 	}
 }
