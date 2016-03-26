f73900c8e278c63e421a1bc1cb4353c2594b4f47
Jesusmann <Jesusmann46and2@gmail.com>
2016-03-25 21:25:44 -0500

    Added case statment and try block
    More more more work work zug zug

--- src/edu/txstate/simpleLibJJP66/FrontEnd.java
+++ src/edu/txstate/simpleLibJJP66/FrontEnd.java
@@ -1,7 +1,11 @@
 package edu.txstate.simpleLibJJP66;
 
+import java.util.Scanner;
+
 public class FrontEnd {
 
+	static Scanner in = new Scanner(System.in);
+	
 	public static void introMenu(){
 		System.out.println(
 				"\n1. Switch to user view. \n"
--- src/edu/txstate/simpleLibJJP66/UserView.java
+++ src/edu/txstate/simpleLibJJP66/UserView.java
@@ -1,12 +1,48 @@
 package edu.txstate.simpleLibJJP66;
 
-public class UserView {
+import java.util.InputMismatchException;
 
-	public static void UserViewMenu(){
-		System.out.println(
-				"\nEnter 1 to Search" 
-				+ "\nEnter 2 to Borrow"
-				+ "\nEnter 3 to Return" 
-				+ "\nEnter 4 to exit.");
+public class UserView extends FrontEnd {
+
+	public static void UVMenu() {
+		System.out
+				.println("\nEnter 1 to Search" + "\nEnter 2 to Borrow" + "\nEnter 3 to Return" + "\nEnter 4 to exit.");
+	}
+
+	public static void UVMenuChoice() {
+		boolean running = true;
+		while (running) {
+			//call UVMenu function to display options
+			UVMenu();
+			try {
+				int answer = in.nextInt();
+
+				switch (answer) {
+
+				case 1:
+					// has Backend search lists
+					break;
+				case 2:
+					// brings you to the borrow menu
+					break;
+				case 3:
+					// return item menu
+					break;
+				case 4:
+					// exits
+					running = false;
+					break;
+				default:
+					System.err.println("Please select a number between 1 and 4.");
+				}
+			} catch (InputMismatchException ex) {
+				System.err.println("Input missmatch. Please Try again." + ex);
+				in.nextLine(); // clear the new line character from the input
+			} catch (UnsupportedOperationException ex) {
+				ex.printStackTrace(); // For debugging purposes only.
+			} catch (Exception ex) {
+				System.err.println("An unknown error has occured." + ex);
+			}
+		}
 	}
 }
