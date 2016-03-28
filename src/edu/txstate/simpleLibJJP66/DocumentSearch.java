41faf2d6f69c99e0ef04283f5e89739a4cbf7cae
Jesusmann <Jesusmann46and2@gmail.com>
2016-03-28 16:54:43 -0500

    search strategy title
    help

--- src/edu/txstate/simpleLibJJP66/DocumentSearch.java
+++ src/edu/txstate/simpleLibJJP66/DocumentSearch.java
@@ -2,24 +2,44 @@ package edu.txstate.simpleLibJJP66;
 
 public interface DocumentSearch {
 
-	String info();
+	void search();
 	
 }
 
 class SearchAuthor implements DocumentSearch{
-	public String info(){
-		return Document.getAuthor();
+	public void search(){
 	}
 }
 
 class SearchTitle implements DocumentSearch{
-	public String info(){
-		return Document.getTitle();
+	public void search(){
+		Boolean search = true;
+		Boolean found = false;
+		String output = null;
+		while (search) {
+
+			for (int i = 0; i < ListOfBooks.size(); i++) {
+				Book TempBook = ListOfBooks.get(i);
+				// If statement to compare to title and publisher and author
+				if (TempBook.getTitle().equals(input) || TempBook.getPublisher().equals(input)
+						|| TempBook.findAuthor(input) == true) {
+					output = TempBook.displayBook();
+					System.out.println(output);
+					found = true;
+					// end if statement
+				}
+				// end for loop
+			}
+			// if statement to display if book not found
+			if (!found) {
+				System.out.println("Book not found!");
+			}
+			search = false;
+		}
 	}
 }
 
 class SearchPublisher implements DocumentSearch{
-	public String info(){
-		return Document.getPublisher();
+	public void search(){
 	}
 }
