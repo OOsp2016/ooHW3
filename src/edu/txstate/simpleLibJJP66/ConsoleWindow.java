05972c97b0214e14ca2f87503ba52ce3235c44d0
Jesusmann <Jesusmann46and2@gmail.com>
2016-03-28 19:50:02 -0500

    capitalization erro
    halp

--- src/edu/txstate/simpleLibJJP66/ConsoleWindow.java
+++ src/edu/txstate/simpleLibJJP66/ConsoleWindow.java
@@ -9,7 +9,7 @@ public abstract class ConsoleWindow {
 	protected static Scanner remove = new Scanner (System.in);
 	protected static Scanner delete = new Scanner (System.in);
 	protected static Scanner User = new Scanner (System.in);
-	protected static Scanner inputForUser = new Scanner (System.in);
+	protected static Scanner InputForUser = new Scanner (System.in);
 	
 	protected abstract void printMenu();
 	
